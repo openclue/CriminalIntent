e1a6e16e06ce6237bb19e8915ff748f5c4c07f8b
Daniel Jablonski <d.jablonski@openclue.pl>
2015-03-29 15:50:14 +0200

    Finished chapter 7.

---
+++ app/src/main/java/pl/openclue/criminalintent/Crime.java
@@ -0,0 +1,27 @@
+package pl.openclue.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by Daniel on 2015-03-29.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
